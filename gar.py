38e4d2f8c2f07d831c09a1b6b35b7f2b175e51b2
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2015-09-24 00:54:06 +0300

    First version issue
    First working version.
    * add ability to pass password for private key
    * add ability to set project

--- gar.py
+++ gar.py
@@ -3,22 +3,26 @@
 Allow fast and easy adding reviewers to commit on gerrit host.
 
 Usage:
-    gar.py [-hv] [-k KEY] [-s HOST] [-p PORT] [-u USER] [-a ADDRS] CHANGE_ID
+    gar.py [-hv] [-k KEY] [--keypass KEYPASS] [-s HOST] [-p PORT] [-u USER] [-a ADDRS] [--project PROJECT] CHANGE_ID
 
 Options:
-    -h --help                   show this help
-    -v --version                show version
-    -k KEY, --key KEY           set path to private key [default: ~/.ssh/id_rsa]
-    -s HOST, --host HOST        set remote host address [default: review.openstack.org]
-    -p PORT, --port PORT        set remote port [default: 29418]
-    -u USER, --user USER        username which will connect to remote server [default is your username]
-    -a ADDRS --addresses ADDRS  file with emails of reviewers to be added [default: ~/.gar/reviewers]
+    -h --help             show this help
+    -v --version          show version
+    -k --key KEY          set path to private key [default: ~/.ssh/id_rsa]
+    --keypass KEYPASS     private key password if it exist
+    -s --host HOST        set remote host address [default: review.openstack.org]
+    -p --port PORT        set remote port [default: 29418]
+    -u --user USER        username which will connect to remote server [default is your username]
+    --project PROJECT     gerrit project which consist a commit [default: stackforge/fuel-library]
+    -a --addresses ADDRS  file with emails of reviewers to be added [default: ~/.gar/reviewers]
 
 Arguments:
     CHANGE_ID  gerrit Change-Id number
 
 """
 # TODO(sbog): add groups of users in file with addresses
+# TODO(sbog): add logger, catch ssh return answer properly
+# TODO(sbog): add keyfile with credentials for private key
 
 from docopt import docopt
 import os
@@ -28,21 +32,47 @@ import sys
 
 if __name__ == '__main__':
 
-    args = docopt(__doc__)
+    args = docopt(__doc__, version='0.1')
     print(args)
-    sys.exit(0)
 
-    keyp = '%s/.ssh/id_rsa' % os.environ['HOME']
-    host = 'review.openstack.org'
-    user = 'test' # test it against getpass.getuser later
-    port = 29418
     comm = 'gerrit set-reviewers '
-    addr = 'test@test.com' # get it from config
-    chid = 226806 # get it from command line
+    addrs = ''
 
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(hostname=host, username=user, key_filename=keyp, port=port)
-    stdin, stdout, stderr = client.exec_command('gerrit set-reviewers -a %s 226776' % addr)
+    # Load private key
+    if args['--keypass']:
+        try:
+            client.connect(hostname=args['--host'],
+                           username=args['--user'],
+                           key_filename=os.path.expanduser(args['--key']),
+                           password=args['--keypass'],
+                           port=int(args['--port']))
+        except paramiko.ssh_exception.SSHException:
+            print('Unable to parse you private key. Maybe wrong password or '
+                  'user? Exiting...')
+            sys.exit(1)
+    else:
+        try:
+            client.connect(hostname=args['--host'],
+                           username=args['--user'],
+                           key_filename=os.path.expanduser(args['--key']),
+                           port=int(args['--port']))
+        except paramiko.ssh_exception.PasswordRequiredException:
+            print('Your private key require a password and you did not point '
+                  'it. Exiting...')
+            sys.exit(1)
+
+
+    # Load reviewers from addresses file
+    try:
+        with open(args['--addresses'], 'r') as f:
+           addrs = [line.rstrip() for line in f.readlines()]
+    except FileNotFoundError:
+        print("Sorry, file with reviewers was not found, exiting...")
+        sys.exit(1)
+
+    stdin, stdout, stderr = client.exec_command('gerrit set-reviewers -p %s -a %s %s'
+                              % (args['--project'], ' -a '.join(addrs), args['CHANGE_ID']))
     data = stdout.read() + stderr.read()
     client.close()
