0ced9a45d3347eac28221aa48f9d795a0e60662e
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2015-12-15 02:36:17 +0300

    Added tests

--- tests/test_gar.py
+++ tests/test_gar.py
@@ -1,7 +1,24 @@
 import pytest
 import docopt
+import sys
 from gar.gar import Options, Gar
 
-def test_Options():
-    with pytest.raises(docopt.DocoptExit):
+class TestOptions:
+    def test_without_arguments(self):
+        with pytest.raises(docopt.DocoptExit):
+            o = Options()
+
+    def test_with_change_id(self):
+        sys.argv = '101010'
         o = Options()
+        assert o
+
+    def test_version(self):
+        sys.argv = ' --version'
+        try:
+            o = Options()
+        except:
+            pass
+        assert o.args.get('--version') == True
+
+
